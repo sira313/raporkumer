b451545a2d61707739fec4e65ab9c58ab060ab13
sira313 <si.ecenk@gmail.com>
2025-06-22 12:17:29 +0700

    ui data kelas

---
+++ src/routes/(informasi-umum)/kelas/+page.svelte
@@ -0,0 +1,104 @@
+<form>
+	<fieldset class="fieldset bg-base-100 w-full rounded-lg border border-none p-4 shadow-md">
+		<legend class="fieldset-legend">Formulir Isian Data Kelas</legend>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+			<!-- Nama Kelas -->
+			<div>
+				<legend class="fieldset-legend">Nama Kelas</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: VI (Kelas 6)"
+					name="namaKelas"
+				/>
+			</div>
+
+			<!-- Fase -->
+			<div>
+				<legend class="fieldset-legend">Fase</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: Fase C (Kelas 6)"
+					name="fase"
+				/>
+			</div>
+
+			<!-- Semester -->
+			<div>
+				<legend class="fieldset-legend">Semester</legend>
+				<select
+					class="select bg-base-200 validator w-full border dark:border-none"
+					name="semester"
+					required
+				>
+					<option value="" disabled selected>Pilih Semester</option>
+					<option>Ganjil</option>
+					<option>Genap</option>
+				</select>
+			</div>
+
+			<!-- Tahun Ajaran -->
+			<div>
+				<legend class="fieldset-legend">Tahun Ajaran</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: 2025/2026"
+					name="tahunAjaran"
+				/>
+			</div>
+			<!-- Nama Kepala Sekolah -->
+			<div>
+				<legend class="fieldset-legend">Nama Kepala Sekolah</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: Bruce Wayne, Bat"
+					name="namaKepalaSekolah"
+				/>
+			</div>
+
+			<!-- NIP Kepala Sekolah -->
+			<div>
+				<legend class="fieldset-legend">NIP Kepala Sekolah</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: 19700305 199309 1 009"
+					name="nipKepalaSekolah"
+				/>
+			</div>
+
+			<!-- Wali Kelas -->
+			<div>
+				<legend class="fieldset-legend">Wali Kelas</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: Damian Wayne, Bat"
+					name="waliKelas"
+				/>
+			</div>
+
+			<!-- NIP Wali Kelas -->
+			<div>
+				<legend class="fieldset-legend">NIP Wali Kelas</legend>
+				<input
+					required
+					type="text"
+					class="input validator bg-base-200 w-full dark:border-none"
+					placeholder="Contoh: 19940505 201803 1 008"
+					name="nipWaliKelas"
+				/>
+			</div>
+		</div>
+		<button class="btn btn-primary mt-6 ml-auto shadow-none"> Simpan </button>
+	</fieldset>
+</form>
---
+++ src/routes/(informasi-umum)/kelas/data-kelas/+page.svelte
@@ -0,0 +1,42 @@
+<form>
+	<fieldset class="fieldset bg-base-100 w-full rounded-lg border border-none p-4 shadow-md">
+		<legend class="fieldset-legend">Formulir Isian Data Kelas</legend>
+		<div class="bg-base-100 rounded-box grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
+			<!-- Kolom 1 -->
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Nama Kelas</div>
+				<div class="text-lg font-medium">VI (Kelas 6)</div>
+			</div>
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Fase</div>
+				<div class="text-lg font-medium">Fase C</div>
+			</div>
+
+			<!-- Kolom 2 -->
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Semester</div>
+				<div class="text-lg font-medium">Ganjil</div>
+			</div>
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Tahun Ajaran</div>
+				<div class="text-lg font-medium">2025/2026</div>
+			</div>
+
+			<!-- Kolom 3 -->
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Kepala Sekolah</div>
+				<div class="text-lg font-medium">Bruce Wayne, Bat</div>
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">NIP 19700305 199309 1 009</div>
+			</div>
+
+			<!-- Kolom 4 -->
+			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">Wali Kelas</div>
+				<div class="text-lg font-medium">Damian Wayne, Bat</div>
+				<div class="text-base-content/70 mb-1 text-sm font-semibold">NIP 19940505 201803 1 008</div>
+			</div>
+		</div>
+
+		<a href="/kelas" class="btn btn-primary mt-6 ml-auto shadow-none"> Edit </a>
+	</fieldset>
+</form>
