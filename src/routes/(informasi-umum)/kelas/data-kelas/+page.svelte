2ce053ce44926479b0f9284d6f3aa5e85b859784
Apoxicam <45110036+sira313@users.noreply.github.com>
2025-06-22 18:41:00 +0700

    UI/dataSekolah (#28)
    * fix theme
    
    * UI siswa
    
    * fix layout
    
    * hapus tombol x di modal
    
    * menu tambah siswa manual
    
    * overflow-y mobile
    
    * Dummy data
    
    * feature request
    
    * add feature swap theme
    
    * tombol swap theme
    
    * ganti ke tabs
    
    * perbaiki tabs error dan menambahkan modal dialog konformasi del
    
    * ui data kelas dan data sekolah

--- src/routes/(informasi-umum)/kelas/data-kelas/+page.svelte
+++ src/routes/(informasi-umum)/kelas/data-kelas/+page.svelte
@@ -1,42 +1,48 @@
-<form>
-	<fieldset class="fieldset bg-base-100 w-full rounded-lg border border-none p-4 shadow-md">
-		<legend class="fieldset-legend">Formulir Isian Data Kelas</legend>
-		<div class="bg-base-100 rounded-box grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
-			<!-- Kolom 1 -->
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Nama Kelas</div>
-				<div class="text-lg font-medium">VI (Kelas 6)</div>
-			</div>
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Fase</div>
-				<div class="text-lg font-medium">Fase C</div>
-			</div>
+<fieldset class="fieldset bg-base-100 mx-auto w-full max-w-3xl rounded-lg p-4 shadow-md">
+	<legend class="fieldset-legend">Data Kelas</legend>
 
-			<!-- Kolom 2 -->
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Semester</div>
-				<div class="text-lg font-medium">Ganjil</div>
-			</div>
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Tahun Ajaran</div>
-				<div class="text-lg font-medium">2025/2026</div>
-			</div>
-
-			<!-- Kolom 3 -->
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Kepala Sekolah</div>
-				<div class="text-lg font-medium">Bruce Wayne, Bat</div>
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">NIP 19700305 199309 1 009</div>
-			</div>
-
-			<!-- Kolom 4 -->
-			<div class="bg-base-200 border-base-300 rounded-md border p-4 dark:border-none">
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">Wali Kelas</div>
-				<div class="text-lg font-medium">Damian Wayne, Bat</div>
-				<div class="text-base-content/70 mb-1 text-sm font-semibold">NIP 19940505 201803 1 008</div>
+	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+		<div class="card bg-base-200">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Nama Kelas</h2>
+				<p class="text-lg font-semibold">VI (Kelas 6)</p>
 			</div>
 		</div>
+		<div class="card bg-base-200">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Fase</h2>
+				<p class="text-lg font-semibold">Fase C</p>
+			</div>
+		</div>
+		<div class="card bg-base-200">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Semester</h2>
+				<p class="text-lg font-semibold">Ganjil</p>
+			</div>
+		</div>
+		<div class="card bg-base-200">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Tahun Ajaran</h2>
+				<p class="text-lg font-semibold">2025/2026</p>
+			</div>
+		</div>
+		<div class="card bg-base-200 col-span-full lg:col-span-1">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Kepala Sekolah</h2>
+				<p class="text-lg font-semibold">Bruce Wayne, Bat</p>
+				<p class="text-base-content/70 text-sm">NIP 19700305 199309 1 009</p>
+			</div>
+		</div>
+		<div class="card bg-base-200 col-span-full lg:col-span-1">
+			<div class="card-body p-4">
+				<h2 class="card-title text-base-content/70 text-sm">Wali Kelas</h2>
+				<p class="text-lg font-semibold">Damian Wayne, Bat</p>
+				<p class="text-base-content/70 text-sm">NIP 19940505 201803 1 008</p>
+			</div>
+		</div>
+	</div>
 
-		<a href="/kelas" class="btn btn-primary mt-6 ml-auto shadow-none"> Edit </a>
-	</fieldset>
-</form>
+	<div class="mt-6 text-right">
+		<a href="/kelas" class="btn btn-primary shadow-md">Edit</a>
+	</div>
+</fieldset>
