3ccfceab02b7a9978f454495a32449f3fc90ae6e
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-06-23 12:15:08 +0700

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	preprocess: [mdsvex({ extensions: ['.md'] }), vitePreprocess()],
+	kit: { adapter: adapter() },
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
