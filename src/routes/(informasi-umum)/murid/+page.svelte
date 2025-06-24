5bb0c9b96c9c9fd0f109184a6a77a41994ed72c1
Mustofa <25121822+mustofa-id@users.noreply.github.com>
2025-06-24 12:51:51 +0700

    fix: global modal close behavior (#35)
    * fix close delay and refactor
    
    * use murid loading

--- src/routes/(informasi-umum)/murid/+page.svelte
+++ src/routes/(informasi-umum)/murid/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { showModal } from '$lib/components/modal/state.svelte';
+	import { showModal } from '$lib/components/global-modal.svelte';
 	import { toast } from '$lib/components/toast/state.svelte';
 	import db from '$lib/data/db';
 	import iconDel from '$lib/icons/del.svg?raw';
@@ -96,12 +96,17 @@
 		</button>
 	</div>
 
-	<div class="flex flex-col gap-2 sm:flex-row">
+	<div class="flex flex-col items-center gap-2 sm:flex-row">
 		<!-- Cari nama murid -->
 		<label class="input bg-base-200 dark:border-none">
 			<span>{@html IconSearch}</span>
 			<input type="search" required placeholder="Cari nama murid..." />
 		</label>
+
+		{#if loading}
+			<em class="opacity-50">Loading...</em>
+		{/if}
+
 		<!-- pagination -->
 		<div class="join sm:ml-auto">
 			<button class="join-item btn btn-active">1</button>
