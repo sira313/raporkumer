e3765fa720862585562f05731380da2bc119377d
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-06-26 06:15:52 +0700

    chore: dev script wrapper
    for support parallel execution and args passing

---
+++ scripts/dev.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { spawn } from 'child_process';
+
+const args = process.argv.slice(2);
+const vite_args = ['dev', ...args];
+
+const icon_process = spawn('node', ['scripts/icon.js'], { stdio: 'inherit' });
+const vite_process = spawn('npx', ['vite', ...vite_args], { stdio: 'inherit' });
+
+/** @param {string} name */
+const on_exit = (name) => {
+	return /** @param {number} code */ (code) => {
+		if (code !== 0) {
+			console.error(`Process "${name}" exited with code ${code}`);
+			process.exit(code);
+		}
+	};
+};
+
+icon_process.on('exit', on_exit('icon'));
+vite_process.on('exit', on_exit('vite'));
