d095821096aa6b1fa10e6f9d27f0669bd1429441
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-06-25 07:47:24 +0700

    run watcher

--- scripts/icon.js
+++ scripts/icon.js
@@ -3,24 +3,29 @@
 import { readdir, watch, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
+const types_file = '__icons.d.ts'; // remember to gitignore it
 const icons_dir = `./src/lib/icons`;
 const watch_ctrl = new AbortController();
 
 async function generate_types() {
 	const files = await readdir(icons_dir);
+	const comment = `// this file is generated — do not edit it\n\n`;
 	const svg_files_union = files
 		.filter((f) => path.extname(f).toLocaleLowerCase() == '.svg')
 		.map((t) => `'${path.parse(t).name}'\n`)
 		.join('\t| ');
-	const raw = `type IconName =\n\t| ${svg_files_union.trimEnd()};\n`;
-	await writeFile('./src/lib/components/icon.d.ts', raw, 'utf8');
+	const content = `${comment}type IconName =\n\t| ${svg_files_union.trimEnd()};\n`;
+	await writeFile(path.join(icons_dir, types_file), content, 'utf8');
 }
 
 async function run_watcher() {
 	try {
+		let timer;
 		const watcher = watch(icons_dir, { signal: watch_ctrl.signal });
 		for await (const event of watcher) {
-			console.log(event);
+			if (event.filename == types_file) continue;
+			clearTimeout(timer);
+			timer = setTimeout(generate_types, 700);
 		}
 	} catch (err) {
 		if (err.name === 'AbortError') {
@@ -32,7 +37,6 @@ async function run_watcher() {
 }
 
 const shutdown = () => {
-	console.log('\nShutting down Icon type-gen...');
 	watch_ctrl.abort();
 };
 
