1a8b4a2d90cc1bc5a82c499347c9b4ec5cff4b60
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-06-25 08:04:46 +0700

    icon placeholder

--- src/lib/components/icon.svelte
+++ src/lib/components/icon.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <!-- this will even better when Svelte Async Component released -->
-{#await import(`$lib/icons/${name}.svg?raw`) then { default: Icon }}
+{#await import(`$lib/icons/${name}.svg?raw`)}
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-3.5 w-3.5"> </svg>
+{:then { default: Icon }}
 	{@html Icon}
 {/await}
