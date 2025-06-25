ae2ac405bf854ffce4f0b06784bc994474fd2e5e
sira313 <si.ecenk@gmail.com>
2025-06-26 01:49:46 +0700

    modal asesmen formatif

--- src/routes/(input-nilai)/asesmen-formatif/+page.svelte
+++ src/routes/(input-nilai)/asesmen-formatif/+page.svelte
@@ -17,15 +17,15 @@
 	<legend class="fieldset-legend">Formulir Asesmen Formatif</legend>
 
 	<div class="flex flex-col items-center gap-2 sm:flex-row">
-		<!-- Cari nama murid -->
-		<label class="input bg-base-200 dark:border-none">
-			<input type="search" required placeholder="Cari nama murid..." />
-		</label>
-
+		<!-- pili mapel dulu, wajib! -->
 		<select class="select bg-base-200 dark:border-none" title="Pilih kelas">
 			<option value="" disabled selected> Pilih Mata Pelajaran </option>
 			<option value=""> IPAS </option>
 		</select>
+		<!-- Cari nama murid -->
+		<label class="input bg-base-200 dark:border-none">
+			<input type="search" required placeholder="Cari nama murid..." />
+		</label>
 
 		<!-- pagination -->
 		<div class="join sm:ml-auto">
@@ -55,17 +55,72 @@
 					<td>1</td>
 					<td>Ali</td>
 					<td>
-						<button class="btn btn-sm btn-soft shadow-none" title="Edit nilai">
+						<button
+							class="btn btn-sm btn-soft shadow-none"
+							title="Edit nilai"
+							on:click={() => (showModal = true)}
+						>
 							<Icon name="edit" />
 							Nilai
 						</button>
 					</td>
 					<td
-						>Baik dalam lingkup materi 1. Perlu pendalaman dalam lingkup materi 2. Perlu bimbingan
-						dalam lingkup materi 3. Sangat baik dalam lingkup materi 4</td
+						>Baik dalam lingkup materi 1, perlu pendalaman dalam lingkup materi 2, perlu bimbingan
+						dalam lingkup materi 3, dan sangat baik dalam lingkup materi 4</td
 					>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </fieldset>
+
+<!-- Modal DaisyUI -->
+{#if showModal}
+	<div class="modal modal-open" id="modal-nilai-formatif">
+		<fieldset class="fieldset modal-box p-4 sm:w-full sm:max-w-2xl">
+			<legend class="fieldset-legend">
+				<pre class="bg-base-200 rounded-xl px-2">Formulir Asesmen Formatif</pre>
+			</legend>
+			<div class="max-h-[60vh] overflow-y-auto sm:max-h-none sm:overflow-y-visible">
+				<h3 class="pb-2 text-lg font-bold">
+					Apakah siswa telah memenuhi setiap tujuan pembelajaran di bawah ini?
+				</h3>
+				<div
+					class="bg-base-100 dark:bg-base-200 border-base-200 mt-4 overflow-x-auto rounded-md shadow-md dark:shadow-none"
+				>
+					<table class="border-base-200 table border dark:border-none">
+						<!-- head -->
+						<thead>
+							<tr class="bg-base-200 dark:bg-base-300 text-base-content text-left font-bold">
+								<th>No</th>
+								<th class="w-full" style="min-width: 100px;">Tujuan Pembelajaran</th>
+								<th>Ya</th>
+								<th>Tidak</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<!-- Semua tujuan pembelajaran dari semua lingkup materi yang di-input ditampilkan -->
+								<td>
+									Mengidentifikasi ciri-ciri makhluk hidup dan memahami pentingnya pelestarian
+									lingkungan tempat hidup makhluk hidup.
+								</td>
+								<td>
+									<input type="checkbox" checked class="checkbox" />
+								</td>
+								<td>
+									<input type="checkbox" class="checkbox" />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="modal-action">
+				<button class="btn shadow-none" on:click={() => (showModal = false)}>Tutup</button>
+				<button class="btn btn-primary shadow-none"> Simpan </button>
+			</div>
+		</fieldset>
+	</div>
+{/if}
