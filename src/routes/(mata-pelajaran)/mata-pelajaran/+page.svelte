3e6ed844be012b09953f96a21be4a0237fa05983
sira313 <si.ecenk@gmail.com>
2025-07-11 02:32:31 +0700

    fix unresponsive layout mata-pelajaran

--- src/routes/(mata-pelajaran)/mata-pelajaran/+page.svelte
+++ src/routes/(mata-pelajaran)/mata-pelajaran/+page.svelte
@@ -8,159 +8,155 @@
 	let { data } = $props();
 </script>
 
-<div class="mx-auto grid max-w-4xl gap-4">
-	<!-- Data Mapel -->
-	<div class="card bg-base-100 rounded-box mx-auto w-full border border-none p-4 shadow-md">
-		<h2 class="mb-6 text-xl font-bold">Daftar Mata Pelajaran</h2>
-		<form data-sveltekit-keepfocus data-sveltekit-replacestate use:autoSubmit>
-			<select
-				class="select bg-base-200 mb-2 w-full dark:border-none"
-				title="Pilih kelas"
-				name="kelas_id"
-				value={data.kelasId || ''}
-			>
-				<option value="" disabled selected> Pilih Kelas </option>
-				{#each data.daftarKelas as kelas (kelas)}
-					<option value={kelas.id + ''}>Kelas: {kelas.nama} - Fase: {kelas.fase}</option>
+<!-- Data Mapel -->
+<div class="card bg-base-100 mx-auto w-full max-w-4xl rounded-lg border border-none p-4 shadow-md">
+	<h2 class="mb-6 text-xl font-bold">Daftar Mata Pelajaran</h2>
+	<form data-sveltekit-keepfocus data-sveltekit-replacestate use:autoSubmit>
+		<select
+			class="select bg-base-200 mb-2 w-full dark:border-none"
+			title="Pilih kelas"
+			name="kelas_id"
+			value={data.kelasId || ''}
+		>
+			<option value="" disabled selected> Pilih Kelas </option>
+			{#each data.daftarKelas as kelas (kelas)}
+				<option value={kelas.id + ''}>Kelas: {kelas.nama} - Fase: {kelas.fase}</option>
+			{:else}
+				<option value="" disabled selected> Belum ada data kelas </option>
+			{/each}
+		</select>
+	</form>
+	<div class="flex flex-col gap-2 sm:flex-row">
+		<!-- tombol tambah mapel -->
+		<a
+			class="btn mb-2 shadow-none sm:max-w-40"
+			href="/mata-pelajaran/form"
+			use:modalRoute={'add-mapel'}
+		>
+			<Icon name="plus" />
+			Tambah
+		</a>
+		<!-- tombol download template -->
+		<button class="btn mb-2 shadow-none">
+			<Icon name="download" />
+			Download Template
+		</button>
+		<!-- tombol import -->
+		<button class="btn mb-2 shadow-none">
+			<Icon name="import" />
+			Import
+		</button>
+		<!-- Tombol ini hanya aktif bila user centang mapel untuk hapus -->
+		<button disabled class="btn btn-error mb-2 shadow-none sm:ml-auto sm:max-w-40">
+			<Icon name="del" />
+			Hapus
+		</button>
+	</div>
+	<!-- Tabel Mapel Wajib -->
+	<legend class="fieldset-legend"> Mata Pelajaran Wajib </legend>
+	<div class="bg-base-100 dark:bg-base-200 overflow-x-auto rounded-md shadow-md dark:shadow-none">
+		<table class="border-base-200 table border dark:border-none">
+			<thead>
+				<tr class="bg-base-200 dark:bg-base-300 text-base-content text-left font-bold">
+					<th style="width: 50px; min-width: 40px;"><input type="checkbox" class="checkbox" /></th>
+					<th style="width: 50px; min-width: 40px;">No</th>
+					<th style="width: 50%;">Mata Pelajaran</th>
+					<th>KKM</th>
+					<th>Tujuan Pembelajaran</th>
+					<th>Aksi</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.mapel.daftarWajib as mapel, index (mapel)}
+					<tr>
+						<td><input type="checkbox" class="checkbox" /></td>
+						<td>{index + 1}</td>
+						<td>{mapel.nama}</td>
+						<td>{mapel.kkm}</td>
+						<td class="flex flex-row gap-2">
+							<a
+								href="/mata-pelajaran/{mapel.id}/tp-rl"
+								class="btn btn-sm btn-soft shadow-none"
+								type="button"
+							>
+								<Icon name="edit" />
+								Edit TP
+							</a>
+						</td>
+						<td>
+							<div class="flex flex-row gap-2">
+								<a
+									class="btn btn-sm btn-soft btn-error shadow-none"
+									type="button"
+									href="/mata-pelajaran/{mapel.id}/delete"
+									use:modalRoute={'delete-mapel'}
+								>
+									<Icon name="del" />
+									Hapus
+								</a>
+							</div>
+						</td>
+					</tr>
 				{:else}
-					<option value="" disabled selected> Belum ada data kelas </option>
+					<tr>
+						<td class="text-center italic opacity-50" colspan="6">Belum ada data</td>
+					</tr>
 				{/each}
-			</select>
-		</form>
-		<div class="flex flex-col gap-2 sm:flex-row">
-			<!-- tombol tambah mapel -->
-			<a
-				class="btn mb-2 shadow-none sm:max-w-40"
-				href="/mata-pelajaran/form"
-				use:modalRoute={'add-mapel'}
-			>
-				<Icon name="plus" />
-				Tambah
-			</a>
-			<!-- tombol download template -->
-			<button class="btn mb-2 shadow-none">
-				<Icon name="download" />
-				Download Template
-			</button>
-			<!-- tombol import -->
-			<button class="btn mb-2 shadow-none">
-				<Icon name="import" />
-				Import
-			</button>
-			<!-- Tombol ini hanya aktif bila user centang mapel untuk hapus -->
-			<button disabled class="btn btn-error mb-2 shadow-none sm:ml-auto sm:max-w-40">
-				<Icon name="del" />
-				Hapus
-			</button>
-		</div>
-		<!-- Tabel Mapel Wajib -->
-		<legend class="fieldset-legend"> Mata Pelajaran Wajib </legend>
-		<div class="bg-base-100 dark:bg-base-200 overflow-x-auto rounded-md shadow-md dark:shadow-none">
-			<table class="border-base-200 table border dark:border-none">
-				<thead>
-					<tr class="bg-base-200 dark:bg-base-300 text-base-content text-left font-bold">
-						<th style="width: 50px; min-width: 40px;"><input type="checkbox" class="checkbox" /></th
-						>
-						<th style="width: 50px; min-width: 40px;">No</th>
-						<th style="width: 50%;">Mata Pelajaran</th>
-						<th>KKM</th>
-						<th>Tujuan Pembelajaran</th>
-						<th>Aksi</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each data.mapel.daftarWajib as mapel, index (mapel)}
-						<tr>
-							<td><input type="checkbox" class="checkbox" /></td>
-							<td>{index + 1}</td>
-							<td>{mapel.nama}</td>
-							<td>{mapel.kkm}</td>
-							<td class="flex flex-row gap-2">
+			</tbody>
+		</table>
+	</div>
+	<!-- Data Muatan lokal -->
+	<legend class="fieldset-legend mt-2"> Muatan Lokal </legend>
+	<div class="bg-base-100 dark:bg-base-200 overflow-x-auto rounded-md shadow-md dark:shadow-none">
+		<table class="border-base-200 table border dark:border-none">
+			<thead>
+				<tr class="bg-base-200 dark:bg-base-300 text-base-content text-left font-bold">
+					<th style="width: 50px; min-width: 40px;"><input type="checkbox" class="checkbox" /></th>
+					<th style="width: 50px; min-width: 40px;">No</th>
+					<th style="width: 50%;">Mata Pelajaran</th>
+					<th>KKM</th>
+					<th>Tujuan Pembelajaran</th>
+					<th>Aksi</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.mapel.daftarMulok as mapel, index (mapel)}
+					<tr>
+						<td><input type="checkbox" class="checkbox" /></td>
+						<td>{index + 1}</td>
+						<td>{mapel.nama}</td>
+						<td>{mapel.kkm}</td>
+						<td class="flex flex-row gap-2">
+							<a
+								href="/mata-pelajaran/{mapel.id}/tp-rl"
+								class="btn btn-sm btn-soft shadow-none"
+								type="button"
+							>
+								<Icon name="edit" />
+								Edit TP
+							</a>
+						</td>
+						<td>
+							<div class="flex flex-row gap-2">
 								<a
-									href="/mata-pelajaran/{mapel.id}/tp-rl"
-									class="btn btn-sm btn-soft shadow-none"
+									class="btn btn-sm btn-soft btn-error shadow-none"
 									type="button"
+									href="/mata-pelajaran/{mapel.id}/delete"
+									use:modalRoute={'delete-mapel'}
 								>
-									<Icon name="edit" />
-									Edit TP
+									<Icon name="del" />
+									Hapus
 								</a>
-							</td>
-							<td>
-								<div class="flex flex-row gap-2">
-									<a
-										class="btn btn-sm btn-soft btn-error shadow-none"
-										type="button"
-										href="/mata-pelajaran/{mapel.id}/delete"
-										use:modalRoute={'delete-mapel'}
-									>
-										<Icon name="del" />
-										Hapus
-									</a>
-								</div>
-							</td>
-						</tr>
-					{:else}
-						<tr>
-							<td class="text-center italic opacity-50" colspan="6">Belum ada data</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
-		<!-- Data Muatan lokal -->
-		<legend class="fieldset-legend mt-2"> Muatan Lokal </legend>
-		<div class="bg-base-100 dark:bg-base-200 overflow-x-auto rounded-md shadow-md dark:shadow-none">
-			<table class="border-base-200 table border dark:border-none">
-				<thead>
-					<tr class="bg-base-200 dark:bg-base-300 text-base-content text-left font-bold">
-						<th style="width: 50px; min-width: 40px;"><input type="checkbox" class="checkbox" /></th
-						>
-						<th style="width: 50px; min-width: 40px;">No</th>
-						<th style="width: 50%;">Mata Pelajaran</th>
-						<th>KKM</th>
-						<th>Tujuan Pembelajaran</th>
-						<th>Aksi</th>
+							</div>
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					{#each data.mapel.daftarMulok as mapel, index (mapel)}
-						<tr>
-							<td><input type="checkbox" class="checkbox" /></td>
-							<td>{index + 1}</td>
-							<td>{mapel.nama}</td>
-							<td>{mapel.kkm}</td>
-							<td class="flex flex-row gap-2">
-								<a
-									href="/mata-pelajaran/{mapel.id}/tp-rl"
-									class="btn btn-sm btn-soft shadow-none"
-									type="button"
-								>
-									<Icon name="edit" />
-									Edit TP
-								</a>
-							</td>
-							<td>
-								<div class="flex flex-row gap-2">
-									<a
-										class="btn btn-sm btn-soft btn-error shadow-none"
-										type="button"
-										href="/mata-pelajaran/{mapel.id}/delete"
-										use:modalRoute={'delete-mapel'}
-									>
-										<Icon name="del" />
-										Hapus
-									</a>
-								</div>
-							</td>
-						</tr>
-					{:else}
-						<tr>
-							<td class="text-center italic opacity-50" colspan="6">Belum ada data</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
+				{:else}
+					<tr>
+						<td class="text-center italic opacity-50" colspan="6">Belum ada data</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
 
