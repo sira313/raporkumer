661ec321ce0175633aec8bd7c2e734cea21899eb
sira313 <si.ecenk@gmail.com>
2025-07-13 07:08:51 +0700

    hapus fieldset title di laman asesmen-sumatif

--- src/routes/(input-nilai)/asesmen-sumatif/+page.svelte
+++ src/routes/(input-nilai)/asesmen-sumatif/+page.svelte
@@ -2,10 +2,7 @@
 	import Icon from '$lib/components/icon.svelte';
 </script>
 
-<fieldset
-	class="fieldset bg-base-100 mx-auto w-full max-w-4xl rounded-lg border border-none p-4 shadow-md"
->
-	<legend class="fieldset-legend">Formulir Asesmen Sumatif</legend>
+<div class="card bg-base-100 mx-auto w-full max-w-4xl rounded-lg border border-none p-4 shadow-md">
 	<div class="mb-2 flex flex-col gap-2 sm:flex-row">
 		<button class="btn shadow-none">
 			<!-- download template excel -->
@@ -76,7 +73,22 @@
 						</a>
 					</td>
 				</tr>
+				<tr>
+					<td>2</td>
+					<td>Benny</td>
+					<td> 0 </td>
+					<td>
+						<a
+							class="btn btn-sm btn-soft shadow-none"
+							title="Edit nilai"
+							href="/asesmen-sumatif/formulir-asesmen"
+						>
+							<Icon name="edit" />
+							Nilai
+						</a>
+					</td>
+				</tr>
 			</tbody>
 		</table>
 	</div>
-</fieldset>
+</div>
