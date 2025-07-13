5749ba233c487e0d1c9cfb56bfb6eb84c3d48fad
sira313 <si.ecenk@gmail.com>
2025-07-13 07:19:05 +0700

    hapus filedset title di laman absen

--- src/routes/(input-nilai)/absen/+page.svelte
+++ src/routes/(input-nilai)/absen/+page.svelte
@@ -2,11 +2,7 @@
 	import Icon from '$lib/components/icon.svelte';
 </script>
 
-<fieldset
-	class="fieldset bg-base-100 mx-auto w-full max-w-4xl rounded-lg border border-none p-4 shadow-md"
->
-	<legend class="fieldset-legend">Formulir Asesmen Sumatif</legend>
-
+<div class="card bg-base-100 mx-auto w-full max-w-4xl rounded-lg border border-none p-4 shadow-md">
 	<div class="flex flex-col items-center gap-2 sm:flex-row">
 		<!-- pilih kelas, wajib -->
 		<select class="select bg-base-200 w-full dark:border-none" title="Pilih kelas">
@@ -60,7 +56,22 @@
 						</button>
 					</td>
 				</tr>
+				<tr>
+					<td>1</td>
+					<td>Benny</td>
+					<td> 0 </td>
+					<td> 0 </td>
+					<td> 0 </td>
+					<td>
+						<!-- Ketika tombol ini di-klik maka isian cell berubah menjadi input -->
+						<!-- Request fitur: tombol berubah menjadi tombol save ketika user sedang melakukan input -->
+						<button class="btn btn-sm btn-soft shadow-none">
+							<Icon name="edit" />
+							Edit
+						</button>
+					</td>
+				</tr>
 			</tbody>
 		</table>
 	</div>
-</fieldset>
+</div>
