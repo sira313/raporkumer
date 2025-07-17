9c87beb4579bb8b344b32badac4ada6aa3f2c4a4
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-07-17 14:56:55 +0700

    suppress drizzle warning

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
-import { loadEnv } from 'vite';
-
-const env = loadEnv('', '.', '');
 
 export default defineConfig({
-	out: './drizzle',
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'sqlite',
 	casing: 'snake_case',
-	dbCredentials: { url: env['DB_URL'] || 'file:./data/database.sqlite3' }
+	dbCredentials: { url: process.env.DB_URL }
 });
