e5b7e9d99a0e3e62edfaf627781a65307f3f4951
sira313 <me@apoxi.cam>
2025-10-11 15:18:04 +0700

    chore: add title license page

---
+++ src/routes/lisensi/+page.server.ts
@@ -0,0 +1,7 @@
+export async function load() {
+	const meta: PageMeta = {
+		title: 'Lisensi Aplikasi',
+		description: 'Informasi tentang lisensi aplikasi Rapor Kurikulum Merdeka'
+	};
+	return { meta };
+}
