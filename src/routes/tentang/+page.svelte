213adc86a211fdd710e43fede3c2b38732c7bb99
sira313 <me@apoxi.cam>
2025-12-14 18:35:21 +0700

    fix link

--- src/routes/tentang/+page.svelte
+++ src/routes/tentang/+page.svelte
@@ -117,14 +117,14 @@
 					</li>
 					<li>
 						<a
-							href="https://kurikulum.kemdikbud.go.id/file/1753779343_manage_file.pdf"
+							href="https://kurikulum.kemendikdasmen.go.id/file/1753779343_manage_file.pdf"
 							target="_blank"
 							class="link link-primary">Panduan Kokurikuler tahun 2025</a
 						>
 					</li>
 					<li>
 						<a
-							href="https://kurikulum.kemdikbud.go.id/file/1753929861_manage_file.pdf"
+							href="https://drive.google.com/file/d/1XigY7sH6SkA0RCyn_8X85NGJoR4HmyC0/view"
 							target="_blank"
 							class="link link-primary">Capaian Pembelajaran</a
 						>
